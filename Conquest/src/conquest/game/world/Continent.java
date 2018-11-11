3d732619bad3e745058de6c1c7f548cdc0dc26c7
Adam Dingle <adam@medovina.org>
2018-11-11 12:25:30 +0100

    Add continent names

--- Conquest/src/conquest/game/world/Continent.java
+++ Conquest/src/conquest/game/world/Continent.java
@@ -8,12 +8,12 @@ import java.util.Set;
 
 public enum Continent {
 	
-	North_America(1, 5),
-	South_America(2, 2),
-	Europe(3, 5),
-	Africa(4, 3),	
-	Asia(5, 7),
-	Australia(6, 2);
+	North_America("North America", 1, 5),
+	South_America("South America", 2, 2),
+	Europe("Europe", 3, 5),
+	Africa("Africa", 4, 3),	
+	Asia("Asia", 5, 7),
+	Australia("Australia", 6, 2);
 	
 	public static final int LAST_ID = 6;
 	
@@ -23,10 +23,12 @@ public enum Continent {
 	public final int id;
 	public final int reward;
 	public final int continentFlag;
+	public final String mapName;
 	
 	private Set<Region> regions = null;
 
-	private Continent(int id, int reward) {
+	private Continent(String mapName, int id, int reward) {
+		this.mapName = mapName;
 		this.id = id;
 		this.reward = reward;	
 		this.continentFlag = 1 << (id-1);
